0c69453f7b33a01e4bdacff3d79907928fccc27e
Denis Voitenko <dv.voitenko@gmail.com>
2025-03-20 22:40:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'services/cat_service.dart';
 import 'models/cat.dart';
 import 'screens/cat_detail_screen.dart';
+import 'buttons/like_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -121,25 +122,6 @@ class CatTinderScreenState extends State<CatTinderScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class LikeButton extends StatelessWidget {
-  final VoidCallback onTap;
-  final IconData icon;
-
-  const LikeButton({super.key, required this.onTap, required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        padding: const EdgeInsets.all(16),
-      ),
-      child: Icon(icon, size: 32),
     );
   }
 }
