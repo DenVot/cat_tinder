26539d96c748c24eae5ed25f9b8d45bd2670dae6
Denis Vojtenko <vojtenkoda@mts.ru>
2025-03-20 22:23:25 +0300

    Fix bugs

--- lib/models/cat.dart
+++ lib/models/cat.dart
@@ -2,8 +2,11 @@ class Cat {
   final String id;
   final String url;
   final String breedName;
+  final String weight;
+  final String height;
+  final String lifeSpan;
 
-  Cat({required this.id, required this.url, required this.breedName});
+  Cat({required this.id, required this.url, required this.breedName, required this.weight, required this.height, required this.lifeSpan});
 
   factory Cat.fromJson(Map<String, dynamic> json) {
     return Cat(
@@ -12,6 +15,15 @@ class Cat {
       breedName:
           json['breeds'] != null && json['breeds'].isNotEmpty
               ? json['breeds'][0]['name']
+              : 'Unknown',
+      weight: json['breeds'] != null && json['breeds'].isNotEmpty
+              ? json['breeds'][0]['weight']
+              : 'Unknown',
+      height: json['breeds'] != null && json['breeds'].isNotEmpty
+              ? json['breeds'][0]['height']
+              : 'Unknown',
+      lifeSpan: json['breeds'] != null && json['breeds'].isNotEmpty
+              ? json['breeds'][0]['life_span']
               : 'Unknown',
     );
   }
